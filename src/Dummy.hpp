445a5e264105d438f1e0a8d88be24bcc8b22fe58
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-04-26 14:28:51 -0300

    Initial commit

---
+++ src/Dummy.hpp
@@ -0,0 +1,18 @@
+#ifndef _DUMMYPROJECT_DUMMY_HPP_
+#define _DUMMYPROJECT_DUMMY_HPP_
+
+namespace rock_library_mode
+{
+    class DummyClass
+    {
+        public: 
+            /**
+             * Print a welcome to stdout
+             * \return nothing
+             */
+            void welcome();
+    };
+
+} // end namespace rock_library_mode
+
+#endif // _DUMMYPROJECT_DUMMY_HPP_
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <rock_library_mode/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    rock_library_mode::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
